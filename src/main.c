95203ede2f26033a72fb3218bc77043588bd46ba
claudio-vg96 <claudio.uc96@gmail.com>
2017-10-22 21:54:38 -0300

    parseando archivo de configuracion

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /*
 typedef struct
 {
@@ -10,20 +11,57 @@ typedef struct
 */
 int main()
 {
-    FILE *archivo;
+    int ancho = 0; //ancho mapa
+    int largo = 0; //largo mapa
+    char str[20];
+    int linea = 0;
+    int indice = 0;
     char caracteres[100];
+    char *ptr;
+    FILE *archivo;
     archivo = fopen("init.txt","r");
-    if (archivo == NULL)
+    if (archivo == NULL){
+        printf("No se ha podido abrir el archivo\n");
         exit(1);
-    else
+    }
+    while(!feof(archivo))
     {
-        printf("\nEl contenido del archivo de prueba es \n\n");
-        while (feof(archivo) == 0)
+        fgets(caracteres,100,archivo);
+        linea ++;
+        ptr = strtok(caracteres," ");
+        while(ptr != NULL)
         {
-            fgets(caracteres,100,archivo);
-            printf("%s",caracteres);
+            if(linea == 1) //linea 1
+            {
+                if(indice == 0)
+                {
+                    strcpy(str,ptr);
+                    ancho = atoi(str);
+                    indice++;
+                }
+                else
+                {
+                    strcpy(str,ptr);
+                    largo = atoi(str);
+                    indice = 0;
+                }
+            }
+                //printf("%s\n", ptr);
+            else if (linea!= 1 && linea != 6 && linea != 7) // linea 2-5
+            {
+                if (indice == 0)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            ptr = strtok(NULL, " ");
         }
     }
-    fclose(archivo);
+    printf("ancho:%d\n",ancho);
+    printf("largo:%d\n",largo);
     return 0;
 }
