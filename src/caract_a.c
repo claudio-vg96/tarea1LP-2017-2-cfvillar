6d78b4b6598e2593a802afe4cdc5bf4aceb7d25b
claudio-vg96 <claudio.uc96@gmail.com>
2017-10-25 00:28:21 -0300

    cambios en el main, se agregan los 4.c, cambios en el Cmake

---
+++ src/caract_a.c
@@ -0,0 +1,39 @@
+#include "genetic_behaviour.h"
+
+void caract_a(char event, int* movement){ //caracteristica genetica
+	if (event == 'a')
+	{
+		movement[0] = -1;
+		movement[1] = 1;
+		movement[2] = 0;
+		movement[3] = 0;
+	}
+	else if (event == 'b')
+	{
+		movement[0] = 0;
+		movement[1] = 0;
+		movement[2] = 1;
+		movement[3] = 0;
+	}
+	else if (event == 'c')
+	{
+		movement[0] = 0;
+		movement[1] = 0;
+		movement[2] = 1;
+		movement[3] = 1;
+	}
+	else if (event == 'd')
+	{
+		movement[0] = -1;
+		movement[1] = 0;
+		movement[2] = 1;
+		movement[3] = 0;
+	}
+	else if (event == 'e')
+	{
+		movement[0] = 1;
+		movement[1] = 0;
+		movement[2] = 0;
+		movement[3] = 1;
+	}
+}
--- src/genetic_behaviour.c
+++
@@ -1,6 +0,0 @@
-#include <genetic_behaviour.h>
-
-void genetic_behaviour(char event, int* movement)
-{
-    int movementt[4];
-}
